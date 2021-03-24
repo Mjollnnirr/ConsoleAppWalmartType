ce00e773e124c64ad1cadee60529524957fb374e
Mjollnnirr <faridkhm@code.edu.az>
2021-03-24 11:06:38 +0400

    cc

--- Homework-ConsoleApp/MarketApp/task/Program.cs
+++ Homework-ConsoleApp/MarketApp/task/Program.cs
@@ -35,6 +35,7 @@ namespace task
             Grocery grocery7 = new Grocery("Absolute", 40, 10);
             Grocery grocery8 = new Grocery("Absolute", 40, 10);
             Grocery grocery9 = new Grocery("Absolute", 40, 10);
+            Grocery grocery10 = new Grocery("Absolute", 40, 10);
             Phones phones1 = new Phones(400, 2, Brands.iPhone, "7", 128);
             Phones phones2 = new Phones(400, 2, Brands.Samsung, "Galaxy S8", 128);
             Phones phones3 = new Phones(400, 2, Brands.BlackBerry, "Some BlackBerry", 28);
